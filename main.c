f9ef84ea7b1de532f0582211f95443df620f2cfb
kyupid <iamscvv@gmail.com>
2023-12-08 00:07:12 +0900

    Update structure in main.c
    이렇게하면 Linked List가 만들어지는 거임

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 
-struct NODE {
+typedef struct NODE {
+    //관리될 데이터
+    int nData;
+
+    //데이터 관리를 위한 포인터
     struct NODE* next;
-};
+} NODE;
 
 int main() {
-    printf("Hello, World!\n");
+    NODE list[5] = { 0 };
+
+    list[0].next = &list[1];
+    list[1].next = &list[2];
+    list[2].next = &list[3];
+    list[3].next = &list[4];
+    list[4].next = NULL; // 0이라고 써도됨
+
     return 0;
 }
 
