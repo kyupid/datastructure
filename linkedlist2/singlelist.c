6055201d72c4c8b966b79c235f939bf38dbb088e
kyupid <iamscvv@gmail.com>
2023-12-08 22:56:49 +0900

    Implement linked list with insert and print functionality
    The updated code includes necessary headers and declares a structure for linked list nodes. The main functionality added includes an 'InsertNewNode' function (for adding nodes to the linked list), and a 'PrintList' function for displaying all elements in the list. These functions are tested in the main method.

--- linkedlist2/singlelist.c
+++ linkedlist2/singlelist.c
@@ -2,7 +2,47 @@
 // Created by Yeonwoo Kim on 2023/12/08.
 //
 #include <stdio.h>
+#include <string.h>
+#include <malloc/_malloc.h>
+
+typedef struct NODE {
+    char szData[64];
+    struct NODE *next;
+} NODE;
+
+NODE *g_pHead = NULL;
+
+/*연결 리스트 전체 데이터 출력*/
+void PrintList(void) {
+    while (g_pHead != NULL) {
+        printf("[%p] %s, next[%p]\n", g_pHead, g_pHead->szData, g_pHead->next);
+        g_pHead = g_pHead->next;
+    }
+}
+
+int InsertNewNode(char *pszData) {
+    NODE *pNode = (NODE *) malloc(sizeof(NODE));
+    memset(pNode, 0, sizeof(NODE));
+    strcpy(pNode->szData, pszData);
+
+    if (g_pHead == NULL) {
+        g_pHead = pNode;
+    } else {
+        pNode->next = g_pHead;
+        g_pHead = pNode;
+    }
+    return 1;
+}
 
 int main() {
+    InsertNewNode("TEST01");
+    PrintList();
+
+    InsertNewNode("TEST02");
+    PrintList();
+
+    InsertNewNode("TEST03");
+    PrintList();
+
     return 0;
 }
