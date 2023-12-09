2589b2f220004bbc4180af9148b6002abd08f92d
kyupid <iamscvv@gmail.com>
2023-12-09 13:04:55 +0900

    Enhanced linked list functionalities in singlelist.c
    The linked list in the file singlelist.c has been significantly extended with operations for printing, releasing memory, finding data, deleting data, and insertion at both the head and tail. Test cases for these operations have also been included in the main function.

--- linkedlist3/singlelist.c
+++ linkedlist3/singlelist.c
@@ -7,11 +7,21 @@
 
 typedef struct NODE {
     char szData[64];
-    struct NODE* next;
+    struct NODE *next;
 } NODE;
 
 // 더미 헤드
 NODE g_head = {0}; // 많은 경우 동적할당을 함
+
+/*연결 리스트 전체 데이터 출력*/
+void PrintList(void) {
+    NODE *pCurrent = g_head.next;
+    while (pCurrent != NULL) {
+        printf("[%p] %s\n", pCurrent, pCurrent->szData);
+        pCurrent = pCurrent->next;
+    }
+    putchar('\n');
+}
 
 int IsEmpty() {
     if (g_head.next == NULL) {
@@ -20,7 +30,7 @@ int IsEmpty() {
     return 0;
 }
 
-int InsertAtHead(char* pszData) {
+int InsertAtHead(char *pszData) {
     NODE *pNode = (NODE *) malloc(sizeof(NODE));
     memset(pNode, 0, sizeof(NODE));
     strcpy(pNode->szData, pszData);
@@ -34,7 +44,7 @@ int InsertAtHead(char* pszData) {
     return 1;
 }
 
-int InsertAtTail(char* pszData) {
+int InsertAtTail(char *pszData) {
     //마지막 노드를 찾는다
     NODE *pTmp = &g_head;
     while (pTmp->next != 0) {
@@ -48,6 +58,98 @@ int InsertAtTail(char* pszData) {
     pTmp->next = pNode;
 }
 
-int main() {
+void ReleaseList(void) {
+    printf("\nReleaseList()\n");
+    NODE *pTmp = g_head.next;
+    while (pTmp != NULL) {
+        NODE *pDelete = pTmp;
+        pTmp = pTmp->next;
 
+        printf("Delete: [%p] %s\n", pDelete, pDelete->szData);
+        free(pDelete);
+    }
+}
+
+int FindData(char *pszData) {
+    NODE *pCurr = g_head.next;
+    NODE *pPrev = &g_head;
+    while (pCurr != NULL) {
+        if (strcmp(pCurr->szData, pszData) == 0) {
+            return 1;
+        }
+        pCurr = pCurr->next;
+        pPrev = pPrev->next;
+    }
+    return 0;
+}
+
+int DeleteDate(char *pszData) {
+    NODE *pCurr = g_head.next;
+    NODE *pPrev = &g_head;
+    while (pCurr != NULL) {
+        if (strcmp(pCurr->szData, pszData) == 0) {
+            //삭제
+            printf("DeleteData() %s\n", pCurr->szData);
+            pPrev->next = pCurr->next;
+            free(pCurr);
+            return 1;
+        }
+        pCurr = pCurr->next;
+        pPrev = pPrev->next;
+    }
+    return 0;
+}
+
+int main() {
+ // List 테스트를 위한 코드
+    puts("*** InsertAtHead() ***");
+    InsertAtHead("TEST01");
+    InsertAtHead("TEST02");
+    InsertAtHead("TEST03");
+    PrintList();
+
+    if (FindData("TEST01") == 1) {
+        printf("FindDAta(): TEST01 found\n");
+    }
+    if (FindData("TEST02") == 1) {
+        printf("FindDAta(): TEST02 found\n");
+    }
+    if (FindData("TEST03") == 1) {
+        printf("FindDAta(): TEST03 found\n");
+    }
+    putchar('\n');
+
+    DeleteDate("TEST01");
+    PrintList();
+    DeleteDate("TEST02");
+    PrintList();
+    DeleteDate("TEST03");
+    PrintList();
+
+    puts("*** InsertAtTail() ***");
+    InsertAtTail("TEST01");
+    InsertAtTail("TEST02");
+    InsertAtTail("TEST03");
+    PrintList();
+
+    if (FindData("TEST01") == 1) {
+        printf("FindDAta(): TEST01 found\n");
+    }
+    if (FindData("TEST02") == 1) {
+        printf("FindDAta(): TEST02 found\n");
+    }
+    if (FindData("TEST03") == 1) {
+        printf("FindDAta(): TEST03 found\n");
+    }
+    putchar('\n');
+
+    DeleteDate("TEST01");
+    PrintList();
+    DeleteDate("TEST02");
+    PrintList();
+    DeleteDate("TEST03");
+    PrintList();
+
+    ReleaseList();
+    return 0;
 }
